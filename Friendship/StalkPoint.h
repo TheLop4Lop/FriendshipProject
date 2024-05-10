dc8b9769fc087322bf6e4f770397827c8fb6a0ae
PedroMtz <thelop4lop@gmail.com>
2024-05-09 18:22:26 -0600

    Target Point sub classes created, this to implement a better logic in the Stalker class.

---
+++ Friendship/StalkPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TargetPoint.h"
+#include "StalkPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRIENDSHIP_API AStalkPoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+};
